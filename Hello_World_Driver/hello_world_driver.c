a66fa7475bb1371f14dec2e6e1e6f9725cfe0142
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-01-13 12:30:04 +0530

    main

--- Hello_World_Driver/hello_world_driver.c
+++ Hello_World_Driver/hello_world_driver.c
@@ -5,12 +5,14 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Vaishnav Sabari Girish");
 MODULE_DESCRIPTION("Prints 'Hello World'");
 
-static int hello_world_module_init (void) {
+static int hello_world_module_init (void) {           //Runs when the kernel is loaded into the system
+  //Kernel is loaded using sudo insmod <filename>.ko
   printk("Hello World !!!! (Vaishnav Sabari Girish)\n");
   return 0;
 }
 
-static void hello_world_module_exit (void) {
+static void hello_world_module_exit (void) {         //Runs when the kernel is removed from the system
+  //Kernel is removed using sudo rmmod <filename>
   printk("Bye Bye\n");
 }
 
